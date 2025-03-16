37c9a6cb19caed2c92dfcc3f8299f6ed70289c29
hamed-rezaee <57184669+hamed-rezaee@users.noreply.github.com>
2025-03-16 12:37:19 +0100

    cleanup main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_logic_gate_simulator/components/logic_components/base_logic_component.dart';
+import 'package:flutter_logic_gate_simulator/components/pin.dart';
 import 'package:flutter_logic_gate_simulator/components/wire.dart';
 import 'package:flutter_logic_gate_simulator/simulator_manager.dart';
 import 'package:flutter_logic_gate_simulator/widgets/background_grid.dart';
@@ -36,143 +37,133 @@ class _SimulatorCanvasState extends State<SimulatorCanvas> {
   @override
   Widget build(BuildContext context) => Scaffold(
     appBar: CustomAppBar(),
-    body: Column(
-      children: [
-        Expanded(
-          child: GestureDetector(
-            onTapUp: (details) {
-              if (_simulatorManager.isDrawingWire) {
-                setState(_simulatorManager.cancelWireDrawing);
-              }
-            },
-            child: DragTarget<BaseLogicComponent>(
-              onWillAcceptWithDetails: (data) => true,
-              onAcceptWithDetails: (data) {
-                final newComponent =
-                    data.data
-                      ..position =
-                          data.offset - const Offset(0, CustomAppBar.height);
+    body: Column(children: [Expanded(child: _buildSimulatorCanvas())]),
+  );
 
-                setState(() => _simulatorManager.addComponent(newComponent));
-              },
-              builder:
-                  (context, candidateData, rejectedData) => Stack(
-                    children: [
-                      const BackgroundGrid(),
-                      ...(_simulatorManager.wires.map(
-                        (wire) => Wire(
-                          startPosition: wire.startPin.position,
-                          endPosition: wire.endPin.position,
-                          isActive: wire.startPin.value,
-                        ),
-                      )),
-                      if (_simulatorManager.isDrawingWire &&
-                          _simulatorManager.wireStartPin != null &&
-                          _simulatorManager.wireEndPosition != null)
-                        Wire(
-                          startPosition:
-                              _simulatorManager.wireStartPin!.position,
-                          endPosition: _simulatorManager.wireEndPosition!,
-                          isActive: _simulatorManager.wireStartPin!.value,
-                        ),
-                      ...(_simulatorManager.components.map((component) {
-                        return Positioned(
-                          left: component.position.dx,
-                          top: component.position.dy,
-                          child: GestureDetector(
-                            onDoubleTap:
-                                () => setState(
-                                  () => _simulatorManager.removeComponent(
-                                    component,
-                                  ),
-                                ),
-                            onPanUpdate: (details) {
-                              setState(() {
-                                component.position += details.delta;
-                                _simulatorManager.calculateAllOutputs();
-                              });
-                            },
-                            child: component.build(
-                              onInputToggle:
-                                  () => setState(
-                                    _simulatorManager.calculateAllOutputs,
-                                  ),
-                              onPinTap: (pin) {
-                                if (!_simulatorManager.isDrawingWire) {
-                                  setState(() {
-                                    _simulatorManager.isDrawingWire = true;
-                                    _simulatorManager.wireStartPin = pin;
-                                    _simulatorManager.wireEndPosition =
-                                        pin.position;
-                                  });
-                                } else if (_simulatorManager.wireStartPin !=
-                                    null) {
-                                  if (_simulatorManager
-                                          .wireStartPin!
-                                          .isOutput &&
-                                      !pin.isOutput) {
-                                    if (_simulatorManager
-                                            .wireStartPin!
-                                            .component !=
-                                        component) {
-                                      setState(() {
-                                        _simulatorManager.wires.add(
-                                          WireModel(
-                                            startPin:
-                                                _simulatorManager.wireStartPin!,
-                                            endPin: pin,
-                                          ),
-                                        );
-                                        _simulatorManager
-                                          ..cancelWireDrawing()
-                                          ..calculateAllOutputs();
-                                      });
-                                    }
-                                  } else if (!_simulatorManager
-                                          .wireStartPin!
-                                          .isOutput &&
-                                      pin.isOutput) {
-                                    if (_simulatorManager
-                                            .wireStartPin!
-                                            .component !=
-                                        component) {
-                                      setState(() {
-                                        _simulatorManager.wires.add(
-                                          WireModel(
-                                            startPin: pin,
-                                            endPin:
-                                                _simulatorManager.wireStartPin!,
-                                          ),
-                                        );
-                                        _simulatorManager
-                                          ..cancelWireDrawing()
-                                          ..calculateAllOutputs();
-                                      });
-                                    }
-                                  } else {
-                                    setState(
-                                      _simulatorManager.cancelWireDrawing,
-                                    );
-                                  }
-                                }
-                              },
-                            ),
-                          ),
-                        );
-                      })),
-                      Align(
-                        alignment: Alignment.bottomCenter,
-                        child: Padding(
-                          padding: const EdgeInsets.all(32),
-                          child: Toolbar(simulatorManager: _simulatorManager),
-                        ),
-                      ),
-                    ],
-                  ),
-            ),
+  Widget _buildSimulatorCanvas() => GestureDetector(
+    onTapUp: (details) {
+      if (_simulatorManager.isDrawingWire) {
+        setState(_simulatorManager.cancelWireDrawing);
+      }
+    },
+    child: DragTarget<BaseLogicComponent>(
+      onWillAcceptWithDetails: (data) => true,
+      onAcceptWithDetails: _handleComponentDrop,
+      builder:
+          (context, candidateData, rejectedData) => Stack(
+            children: [
+              const BackgroundGrid(),
+              ..._buildWires(),
+              if (_simulatorManager.isDrawingWire) _buildActiveWire(),
+              ..._buildComponents(),
+              _buildToolbar(),
+            ],
           ),
-        ),
-      ],
+    ),
+  );
+
+  void _handleComponentDrop(DragTargetDetails<BaseLogicComponent> details) {
+    final newComponent =
+        details.data
+          ..position = details.offset - const Offset(0, CustomAppBar.height);
+
+    setState(() => _simulatorManager.addComponent(newComponent));
+  }
+
+  List<Widget> _buildWires() =>
+      _simulatorManager.wires
+          .map(
+            (wire) => Wire(
+              startPosition: wire.startPin.position,
+              endPosition: wire.endPin.position,
+              isActive: wire.startPin.value,
+            ),
+          )
+          .toList();
+
+  Widget _buildActiveWire() {
+    if (_simulatorManager.wireStartPin == null ||
+        _simulatorManager.wireEndPosition == null) {
+      return const SizedBox.shrink();
+    }
+
+    return Wire(
+      startPosition: _simulatorManager.wireStartPin!.position,
+      endPosition: _simulatorManager.wireEndPosition!,
+      isActive: _simulatorManager.wireStartPin!.value,
+    );
+  }
+
+  List<Widget> _buildComponents() =>
+      _simulatorManager.components
+          .map(
+            (component) => Positioned(
+              left: component.position.dx,
+              top: component.position.dy,
+              child: GestureDetector(
+                onDoubleTap:
+                    () => setState(
+                      () => _simulatorManager.removeComponent(component),
+                    ),
+                onPanUpdate: (details) {
+                  setState(() {
+                    component.position += details.delta;
+                    _simulatorManager.calculateAllOutputs();
+                  });
+                },
+                child: component.build(
+                  onInputToggle:
+                      () => setState(_simulatorManager.calculateAllOutputs),
+                  onPinTap: (pin) => _handlePinTap(pin, component),
+                ),
+              ),
+            ),
+          )
+          .toList();
+
+  void _handlePinTap(Pin pin, BaseLogicComponent component) {
+    if (!_simulatorManager.isDrawingWire) {
+      setState(() {
+        _simulatorManager.isDrawingWire = true;
+        _simulatorManager.wireStartPin = pin;
+        _simulatorManager.wireEndPosition = pin.position;
+      });
+    } else if (_simulatorManager.wireStartPin != null) {
+      _tryConnectWire(pin, component);
+    }
+  }
+
+  void _tryConnectWire(Pin pin, BaseLogicComponent component) {
+    final startPin = _simulatorManager.wireStartPin!;
+
+    if (startPin.component == component) {
+      setState(_simulatorManager.cancelWireDrawing);
+
+      return;
+    }
+
+    setState(() {
+      if (startPin.isOutput && !pin.isOutput) {
+        _simulatorManager.wires.add(WireModel(startPin: startPin, endPin: pin));
+        _simulatorManager
+          ..cancelWireDrawing()
+          ..calculateAllOutputs();
+      } else if (!startPin.isOutput && pin.isOutput) {
+        _simulatorManager.wires.add(WireModel(startPin: pin, endPin: startPin));
+        _simulatorManager
+          ..cancelWireDrawing()
+          ..calculateAllOutputs();
+      } else {
+        _simulatorManager.cancelWireDrawing();
+      }
+    });
+  }
+
+  Widget _buildToolbar() => Align(
+    alignment: Alignment.bottomCenter,
+    child: Padding(
+      padding: const EdgeInsets.all(32),
+      child: Toolbar(simulatorManager: _simulatorManager),
     ),
   );
 }
