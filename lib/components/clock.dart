1e53c30c5af2e6bb359928ede463254683030269
hamed-rezaee <57184669+hamed-rezaee@users.noreply.github.com>
2025-03-16 21:22:20 +0100

    refactor clock component

--- lib/components/clock.dart
+++ lib/components/clock.dart
@@ -5,18 +5,14 @@ import 'package:flutter_logic_gate_simulator/components/components.dart';
 
 class Clock extends BaseLogicComponent {
   Clock({required super.id, required super.position}) {
+    inputPins.add(Pin(component: this, isOutput: false, index: 0));
+
     outputPins.add(Pin(component: this, isOutput: true, index: 0));
   }
 
   final Duration _interval = const Duration(seconds: 1);
 
-  bool isOn = false;
-  bool isRunning = false;
-
   Timer? _timer;
-
-  @override
-  Size get size => const Size(80, 90);
 
   @override
   Widget build({
@@ -25,32 +21,13 @@ class Clock extends BaseLogicComponent {
   }) => ComponentBuilder(
     id: id,
     child: StatefulBuilder(
-      builder:
-          (context, setState) => Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(4),
-                child: Icon(
-                  Icons.timer_sharp,
-                  color: isOn ? Colors.green : Colors.grey,
-                  size: 30,
-                ),
-              ),
-              Transform.scale(
-                scale: 0.7,
-                child: Switch(
-                  value: isRunning,
-                  activeColor: Colors.green,
-                  onChanged: (value) {
-                    isRunning = value;
-                    isRunning ? _startClock(onInputToggle) : _stopClock();
-                    setState(() {});
-                  },
-                ),
-              ),
-            ],
-          ),
+      builder: (context, setState) {
+        setState(() {
+          inputPins[0].value ? _startClock(onInputToggle) : _stopClock();
+        });
+
+        return const Icon(Icons.timer_sharp, color: Colors.white, size: 30);
+      },
     ),
     inputPins: inputPins,
     outputPins: outputPins,
@@ -62,22 +39,25 @@ class Clock extends BaseLogicComponent {
 
   void _startClock(VoidCallback onInputToggle) {
     _timer?.cancel();
+
     _timer = Timer.periodic(_interval, (timer) {
-      isOn = !isOn;
+      outputPins[0].value = !outputPins[0].value;
       onInputToggle();
     });
   }
 
-  @override
-  void calculateOutput() => outputPins[0].value = isOn;
-
-  @override
-  BaseLogicComponent clone() => Clock(position: position, id: id);
-
   void _stopClock() {
+    outputPins[0].value = false;
+
     _timer?.cancel();
     _timer = null;
   }
+
+  @override
+  void calculateOutput() {}
+
+  @override
+  BaseLogicComponent clone() => Clock(position: position, id: id);
 
   @override
   void dispose() => _stopClock();
