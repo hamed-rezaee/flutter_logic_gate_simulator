79d8fe6c00240f5f36e8db0791982d3b66fae047
hamed-rezaee <57184669+hamed-rezaee@users.noreply.github.com>
2025-03-19 19:33:28 +0100

    refactor adder

--- lib/components/advanced_components/adder.dart
+++ lib/components/advanced_components/adder.dart
@@ -3,22 +3,22 @@ import 'package:flutter_logic_gate_simulator/components/components.dart';
 
 class Adder extends BaseLogicComponent with PinNamingMixin {
   Adder({required super.id, required super.position}) {
-    for (var i = 0; i < 5; i++) {
+    for (var i = 0; i < 3; i++) {
       inputPins.add(Pin(index: i, component: this));
     }
 
-    for (var i = 0; i < 3; i++) {
+    for (var i = 0; i < 2; i++) {
       outputPins.add(Pin(index: i, component: this, isOutput: true));
     }
 
     setupDefaultPinNames(
-      inputNames: ['A0', 'A1', 'B0', 'B1', 'Cin'],
-      outputNames: ['S0', 'S1', 'Cout'],
+      inputNames: ['A', 'B', 'Cin'],
+      outputNames: ['S', 'Cout'],
     );
   }
 
   @override
-  Size get size => const Size(110, 95);
+  Size get size => const Size(110, 65);
 
   @override
   Widget build({
@@ -40,26 +40,12 @@ class Adder extends BaseLogicComponent with PinNamingMixin {
 
   @override
   void calculateOutput() {
-    final aInputs = inputPins.sublist(0, 2).map((pin) => pin.value).toList();
-    final bInputs = inputPins.sublist(2, 4).map((pin) => pin.value).toList();
-    final carryIn = inputPins[4].value;
-    final sumOutputs = List<bool>.filled(2, false);
+    final sum = inputPins[0].value ^ inputPins[1].value ^ inputPins[2].value;
+    final carryOut = (inputPins[0].value && inputPins[1].value) ||
+        (inputPins[0].value && inputPins[2].value) ||
+        (inputPins[1].value && inputPins[2].value);
 
-    var carry = carryIn;
-
-    for (var i = 0; i < 2; i++) {
-      final a = aInputs[i];
-      final b = bInputs[i];
-
-      sumOutputs[i] = a ^ b ^ carry;
-
-      carry = (a && b) || (a && carry) || (b && carry);
-    }
-
-    for (var i = 0; i < 2; i++) {
-      outputPins[i].value = sumOutputs[i];
-    }
-
-    outputPins[2].value = carry;
+    outputPins[0].value = sum;
+    outputPins[1].value = carryOut;
   }
 }
