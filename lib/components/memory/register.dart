963486689dee3609c69047523dc6dd870e7f319c
hamed-rezaee <57184669+hamed-rezaee@users.noreply.github.com>
2025-03-19 19:59:07 +0100

    refactor register component

--- lib/components/memory/register.dart
+++ lib/components/memory/register.dart
@@ -3,7 +3,7 @@ import 'package:flutter_logic_gate_simulator/components/components.dart';
 
 class Register extends BaseLogicComponent with PinNamingMixin {
   Register({required super.id, required super.position}) {
-    for (var i = 0; i < 6; i++) {
+    for (var i = 0; i < 8; i++) {
       inputPins.add(Pin(index: i, component: this));
     }
 
@@ -12,7 +12,7 @@ class Register extends BaseLogicComponent with PinNamingMixin {
     }
 
     setupDefaultPinNames(
-      inputNames: ['D0', 'D1', 'D2', 'D3', 'CLK', 'LD'],
+      inputNames: ['D0', 'D1', 'D2', 'D3', 'CLK', 'LD', 'INC', 'RST'],
       outputNames: ['Y0', 'Y1', 'Y2', 'Y3'],
     );
   }
@@ -21,7 +21,7 @@ class Register extends BaseLogicComponent with PinNamingMixin {
   bool previousClock = false;
 
   @override
-  Size get size => const Size(120, 100);
+  Size get size => const Size(120, 130);
 
   @override
   Widget build({
@@ -46,10 +46,18 @@ class Register extends BaseLogicComponent with PinNamingMixin {
     final dataInputs = inputPins.sublist(0, 4).map((pin) => pin.value).toList();
     final clock = inputPins[4].value;
     final load = inputPins[5].value;
+    final increase = inputPins[6].value;
+    final reset = inputPins[7].value;
 
-    if (clock && !previousClock && load) {
-      for (var i = 0; i < storedValues.length; i++) {
-        storedValues[i] = dataInputs[i];
+    if (reset) {
+      _reset();
+    } else if (clock && !previousClock) {
+      if (load) {
+        for (var i = 0; i < storedValues.length; i++) {
+          storedValues[i] = dataInputs[i];
+        }
+      } else if (increase) {
+        _increment();
       }
     }
 
@@ -58,5 +66,27 @@ class Register extends BaseLogicComponent with PinNamingMixin {
     }
 
     previousClock = clock;
+  }
+
+  void _increment() {
+    var value = 0;
+
+    for (var i = 0; i < storedValues.length; i++) {
+      if (storedValues[i]) {
+        value |= 1 << i;
+      }
+    }
+
+    value = (value + 1) % 16;
+
+    for (var i = 0; i < storedValues.length; i++) {
+      storedValues[i] = (value & (1 << i)) != 0;
+    }
+  }
+
+  void _reset() {
+    for (var i = 0; i < storedValues.length; i++) {
+      storedValues[i] = false;
+    }
   }
 }
